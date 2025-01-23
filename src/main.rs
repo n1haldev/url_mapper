c965667786f84dbd5a6930e873b79b51395e88c1
Nihal T M <nihaltmdev@gmail.com>
2025-01-23 12:31:29 +0530

    chore: Added dependencies and integrated modules with program entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,78 @@
-use actix::{System, Actor, Context};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::web::Data;
+use actix_cors::Cors;
+use crate::db::AppState;
+use crate::payload::{ShortenResponse, Longurl};
+use serde::Deserialize;
+use url_shortener::base62_encode;
+use num_bigint::{BigUint, ToBigUint};
+mod payload;
+mod model;
+mod db;
 
-struct actor;
+#[derive(Deserialize)]
+pub struct User {
+    pub name: String,
+    pub age: u8,
+}
 
-impl Actor for actor {
-    type Context = Context<Self>;
+#[derive(Deserialize)]
+pub struct ShortenerRequest {
+    pub url: String,
+}
 
-    fn started(&mut self, _ctx : &mut Self::Context) {
-        println!("I am alive!");
-        // System::current().stop();
-    }
+#[get("/")]
+async fn landing() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
+}
 
-    fn stopped(&mut self, _ctx : &mut Self::Context) {
-        println!("Stopping the server!");
+async fn redirect(data: web::Data<AppState>, path: web::Path<String>) -> impl Responder {
+    let short_url = path.into_inner();
+    if let Some(url_mapping) = data.retreive(short_url).await {
+        HttpResponse::Found()
+            .append_header(("Location", url_mapping.long_url))
+            .finish()
+    } else {
+        HttpResponse::NotFound().body("URL Not Found!")
     }
 }
 
-fn main() {
-    let system = System::new();
+async fn shortener(data: web::Data<AppState>, json: web::Json<Longurl>) -> impl Responder {
+    let url = &json.into_inner().long_url;
+    let bytes = url.as_bytes();
+    
+    // converting byte array to large integer
+    let mut res: BigUint = 0.to_biguint().unwrap();
 
-    let _addr = system.block_on(async { actor.start() });
-    system.run().unwrap();
+    for &byte in bytes {
+        res = res * 256.to_biguint().unwrap() + (byte.to_biguint().unwrap());
+    }
+    
+    let res = base62_encode(res);
+    let short_url = format!("http://localhost:8080/{}", res);
+    data.add(res.clone(), url.clone()).await;
+
+    HttpResponse::Ok().json(ShortenResponse { short_url: short_url, })
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let appstate: Data<AppState> = web::Data::new(AppState::new().await);
+
+    HttpServer::new(move || {
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allow_any_method()
+            .allow_any_header();
+
+        App::new()
+            .app_data(appstate.clone())
+            .wrap(cors)
+            .route("/generate", web::post().to(shortener))
+            .route("/{short_url}", web::get().to(redirect))
+            .service(landing)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
